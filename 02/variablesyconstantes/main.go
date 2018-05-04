92634301b1b959d0dff7b9657574afc2d3bd13ac
unknown <antonymyprog@gmail.com>
2018-05-04 18:27:19 -0500

    Clase 02 primera parte

---
+++ 02/variablesyconstantes/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var nombre, apellido string
+	/*nombre := "Antony"
+	apellido := "Gomez"*/
+	nombre, apellido := "Antony", "Gomez"
+
+	//nombre = "Juan"
+	nombre, edad := "Juan", 20
+
+	fmt.Println(nombre, apellido, edad)
+}
